4ea140c870fee2cd1e3b93e64460f01ff6eb10de
ariuq <78844584+ariuq@users.noreply.github.com>
2023-07-24 15:22:07 +0800

    Add files via upload

---
+++ pampers/app/page.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image"
+import appImage from "../public/assets/images/banner_image_lg.webp"
+import Button from "@components/Button"
+import Link from "next/link"
+
+ 
+ const Home = () => {
+   return (
+     <section className="body_section">
+      <div className="home_banner">
+        <Link href="https://instapage.pampers.com/en-us/app-download-qr-code" className="banner_image">
+      <Image 
+        alt="App image"
+        src={appImage}
+        style={{
+          width: '100vw',
+          height: 'auto',
+        }}/>
+        </Link>
+        <div className="banner_description">
+          <h2>Save on diapers</h2>
+          <p>with Pampers Club</p>
+          <div className="banner_button">
+            <Image src="/assets/icons/button_left.svg" width={41} height={40}/>
+            <Button href="https://instapage.pampers.com/en-us/app-download-qr-code" title="Download App Now"/>
+            <Image src="/assets/icons/button_right.svg" width={41} height={40}/>
+          </div>
+        </div>
+      </div>   
+     </section>
+   )
+ }
+ 
+ export default Home
---
+++ pampers/app/pregnancy/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>pregnancy</div>
+  )
+}
+
+export default page
