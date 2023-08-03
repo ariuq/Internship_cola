25b8af7057e4bb01cf353d73653261475a48c8d3
ariuq <78844584+ariuq@users.noreply.github.com>
2023-08-03 10:10:54 +0800

    Delete pampers directory

--- pampers/app/pampers-club/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>pampers club</div>
-  )
-}
-
-export default page
--- pampers/app/pregnancy/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>pregnancy</div>
-  )
-}
-
-export default page
